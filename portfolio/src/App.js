4da448c93e00eab3db86aa439288b690f0f1f193
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-12-20 16:27:22 +0200

    update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Header/>
+      <Content/>
     </div>
   );
 }
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import '../styles/Header.css';
+
+function Header() {
+  const [offset, setOffset] = useState(0);
+
+  const listener = e => {
+    setOffset(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  });
+  return (
+      <div className="App">
+        <header
+            className="header-background"
+            style={{ backgroundPositionY: offset }}
+        >
+          <section className="info-container" style={{ bottom: offset / 2 }}>
+            <h1 id="name">Ville Pellosniemi</h1>
+            <h3 id="title">FRONT END DEVELOPER</h3>
+          </section>
+        </header>
+      </div>
+  );
+}
+
+export default Header;
