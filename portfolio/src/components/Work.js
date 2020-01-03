be86459ff8af7997d149b47dfada824f62e57e21
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2020-01-03 17:18:22 +0200

    update

--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import '../styles/Work.scss';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class Work extends React.Component {
   render() {
     return (
         <div id="work">
-          <AliceCarousel>
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" className="yours-custom-class" />
-            <img src="/img2" className="yours-custom-class" />
-            <img src="/img3" className="yours-custom-class" />
-            <img src="/img4" className="yours-custom-class" />
-            <img src="/img5" className="yours-custom-class" />
-          </AliceCarousel>
+          <div className="lefty">
+            <h1>hello</h1>
+          </div>
+          <div className="righty">
+            <Carousel id="carousel" autoPlay>
+              <div>
+                <img src="http://wallpaperstock.net/autumn-sunset_wallpapers_25281_1280x720.jpg" alt="test"/>
+              </div>
+              <div>
+                <img src="https://wallpaperaccess.com/full/1078291.jpg" alt="test"/>
+              </div>
+              <div>
+                <img src="https://elevenews.com/wp-content/uploads/2018/02/brexit-iiea-header-16-9-1280x720.jpg" alt="test"/>
+              </div>
+            </Carousel>
+          </div>
         </div>
     );
   }
