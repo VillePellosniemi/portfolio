48f55a5aa84a6bc3a11466ead7125c6c6453f605
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2020-01-07 17:37:54 +0200

    update

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../styles/About.scss';
 import Image from '../images/profile.png';
-import sr from './scrollReveal'
-import { SocialIcon } from 'react-social-icons';
+import sr from './scrollReveal';
+import {SocialIcon} from 'react-social-icons';
 
 class About extends React.Component {
 
@@ -27,27 +27,44 @@ class About extends React.Component {
       easing: 'ease',
     };
 
-    sr.reveal(this.refs.box1, config)
-    sr.reveal(this.refs.box2, config2)
+    sr.reveal(this.refs.box1, config);
+    sr.reveal(this.refs.box2, config2);
   };
 
   render() {
     return (
-      <div id="about-test">
-        <div className="left" ref="box1">
-          <h1 className="about">ABOUT ME</h1>
-          <img className="img-circle" src={Image} alt="test" />
-          <div className="icons">
-            <SocialIcon className="icon" url="https://github.com/VillePellosniemi/" target="_blank"/>
-            <SocialIcon className="icon" url="https://www.linkedin.com/in/ville-pellosniemi-41913b178/" target="_blank"/>
-            <SocialIcon className="icon" url="https://twitter.com/Pelzeri_" target="_blank"/>
+        <div id="about-test">
+          <div className="left" ref="box1">
+            <h1 className="about">ABOUT ME</h1>
+            <img className="img-circle" src={Image} alt="test"/>
+            <div className="icons">
+              <SocialIcon className="icon"
+                          url="https://github.com/VillePellosniemi/"
+                          target="_blank"/>
+              <SocialIcon className="icon"
+                          url="https://www.linkedin.com/in/ville-pellosniemi-41913b178/"
+                          target="_blank"/>
+              <SocialIcon className="icon" url="https://twitter.com/Pelzeri_"
+                          target="_blank"/>
+            </div>
+          </div>
+          <div className="middle"/>
+          <div className="right" ref="box2">
+            <p id="aboutme">Contrary to popular belief, Lorem Ipsum is not
+              simply random text. It has roots in a piece of classical Latin
+              literature from 45 BC, making it over 2000 years old. Richard
+              McClintock, a Latin professor at Hampden-Sydney College in
+              Virginia, looked up one of the more obscure Latin words,
+              consectetur, from a Lorem Ipsum passage, and going through the
+              cites of the word in classical literature, discovered the
+              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+              1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+              and Evil) by Cicero, written in 45 BC. This book is a treatise on
+              the theory of ethics, very popular during the Renaissance. The
+              first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+              from a line in section 1.10.32.</p>
           </div>
         </div>
-        <div className="middle"/>
-        <div className="right" ref="box2">
-          <p id="aboutme">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-        </div>
-      </div>
     );
   }
 }
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -6,6 +6,11 @@ import Menu from '../images/menu.png';
 import Player from '../images/play-button.png';
 import Login from '../images/login.png';
 import Code from '../images/code.png';
+import HTML from '../images/html.png';
+import CSS from '../images/css.png';
+import JS from '../images/js.png';
+import REACT from '../images/react.png';
+import PS from '../images/ps.png';
 
 class Work extends React.Component {
 
@@ -13,7 +18,7 @@ class Work extends React.Component {
 
   componentDidMount = () => {
     const config = {
-      origin: 'right',
+      origin: 'bottom',
       duration: 1200,
       delay: 300,
       distance: '500px',
@@ -28,9 +33,30 @@ class Work extends React.Component {
     return (
         <div id="work">
           <h1 id="skills">SKILLS & WORK</h1>
-          <div className="images" ref="box3">
+          <div className="left2">
+            <ul id="list">
+              <li className="list-item">
+                <img className="ikoni" src={HTML} alt="html"/>
+              </li>
+              <li className="list-item">
+                <img className="ikoni" src={CSS} alt="css"/>
+              </li>
+              <li className="list-item">
+                <img className="ikoni" src={JS} alt="css"/>
+              </li>
+              <li className="list-item">
+                <img className="ikoni" src={REACT} alt="css"/>
+              </li>
+              <li className="list-item">
+                <img className="ikoni" src={PS} alt="css"/>
+              </li>
+            </ul>
+          </div>
+          <div className="middle2" ref="box3">
             <div className="container">
-              <img className="image" src={Menu} alt="menu"/>
+              <a href="https://i.imgur.com/fe0T4nw.png"><img className="image"
+                                                             src={Menu}
+                                                             alt="menu"/></a>
             </div>
             <div className="container">
               <img className="image" src={Player} alt="player"/>
@@ -48,8 +74,8 @@ class Work extends React.Component {
               <img className="image" src={Code} alt="tv-maze"/>
             </div>
           </div>
-          <div className="left2">
-            <p id="skills-text">making it over 2000 years old. Richard
+          <div className="right2">
+            <p>making it over 2000 years old. Richard
               McClintock, a Latin professor at Hampden-Sydney College in
               Virginia, looked up one of the more obscure Latin words,
               consectetur, from a Lorem Ipsum passage, and going through the
