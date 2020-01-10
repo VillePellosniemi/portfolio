c900719a9d2f6fd100a96ca757473549f9889691
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2020-01-10 17:40:49 +0200

    update

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -5,8 +5,18 @@ class Contact extends React.Component {
 
   render() {
     return (
-        <div>
-          <h1>hi</h1>
+        <div id="contact">
+          <div className="upper">
+            <h1 id="contact2">CONTACT ME</h1>
+            <p className="contact-info">Mobile: +358 44 031 1533</p>
+            <p className="contact-info">Email: ville.pellosniemi@outlook.com</p>
+          </div>
+          <div className="lower">
+            <p>Copyright 2020 - Ville Pellosniemi</p>
+            <p>All rights reserved. Made with React.</p>
+            <br/>
+            <a id="source-code" href="https://github.com/VillePellosniemi/portfolio" target="_blank" rel="noopener noreferrer">GitHub Repository & Source Code</a>
+          </div>
         </div>
     );
   }
